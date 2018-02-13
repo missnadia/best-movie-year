223a0351cb66fb9ee713814aca31bed3039f94cb
Nadia Lee <miss.nadia.lee@gmail.com>
2018-02-13 22:17:08 +0000

    add code to environment.rb

---
+++ config/environment.rb
@@ -0,0 +1,7 @@
+require "pry"
+require "nokogiri"
+require "open-uri"
+
+require_relative "../lib/best_movie_year/cli"
+require_relative "../lib/best_movie_year/movie"
+require_relative '../lib/best_movie_year/scraper'

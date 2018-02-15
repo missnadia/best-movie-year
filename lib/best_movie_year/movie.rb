74a6648181e7ba19e809a375a5c45e379f5c561a
Nadia Lee <miss.nadia.lee@gmail.com>
2018-02-15 20:51:28 +0000

    add class and instance methods to code

--- lib/best_movie_year/movie.rb
+++ lib/best_movie_year/movie.rb
@@ -1,3 +1,15 @@
 class BestMovies::Movie
+  attr_accessor :name
 
+  def initialize(name = nil)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find(input)
+    self.all[input-1]
+  end
 end

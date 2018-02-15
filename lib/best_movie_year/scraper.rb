35515769a0103f7e651a322171fe131ae4f3e2a6
Nadia Lee <miss.nadia.lee@gmail.com>
2018-02-15 21:21:06 +0000

    add class method to scraper

--- lib/best_movie_year/scraper.rb
+++ lib/best_movie_year/scraper.rb
@@ -1,3 +1,15 @@
 class BestMovies::Scraper
 
+  def self.scrape_movies(url)
+    movie_list = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/?year=2018"))
+    movies = []
+    movie_list.css("div.btn-group btn-primary-border-dropdown").each do |list|
+      years= list.css(".class dropdown-menu").children.css("a").map { |y| y.attribute("href").value }
+      years.each.do |year|
+        movie_name = year.css(".unstyled articleLink").text
+        movies << {name: movie_name}
+      end
+    end
+  end
+  movies
 end

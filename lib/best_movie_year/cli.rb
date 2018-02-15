149c4368780dd5e50946fc9305c0f40d9a21a52a
Nadia Lee <miss.nadia.lee@gmail.com>
2018-02-15 20:45:55 +0000

    add methods to code

--- lib/best_movie_year/cli.rb
+++ lib/best_movie_year/cli.rb
@@ -8,13 +8,20 @@ class BestMovies::CLI
   def start
     input = nil
     while input != "exit"
-      puts "Please enter a year from 1950 to 2018 to view the top 10 movies released that year:"
-      input = gets.chomp
+      puts "Please enter a year from 1950 to 2018 to view the top 10 movies released that year. To exit, please type 'exit':"
+      input = gets.strip.to_i
       if input > 1949 && input < 2019
-        search_movie
+        if movie = BestMovies::Movie.find(input.to_i)
+          list_movie(movie)
+        end
       else input < 1949 || input > 2018
         start
       end
     end
+    puts "Goodbye."
+  end
+
+  def list_movie(movie)
+    BestMovies::Movie.all.each.with_index(1) { |movie, index| puts "#{index}. #{movie.name}"}
   end
 end

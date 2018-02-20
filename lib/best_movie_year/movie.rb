aaeaeb7183f6d78079ae46ce4e934203d3cabc2b
Nadia Lee <miss.nadia.lee@gmail.com>
2018-02-20 21:24:40 +0000

    delete scraper.rb & merge with movie.rb

--- lib/best_movie_year/movie.rb
+++ lib/best_movie_year/movie.rb
@@ -1,8 +1,8 @@
 class BestMovies::Movie
-  attr_accessor :name, :year
+  attr_accessor :title, :year
 
-  def initialize(name = nil, year = nil)
-    @name = name
+  def initialize(title = nil, year = nil)
+    @title = name
     @year = year
     @@all << self
   end
@@ -13,5 +13,23 @@ class BestMovies::Movie
 
   def self.find(input)
     self.all[input.to_i]
+  end
+
+  def self.scrape_years(url)
+    movie_list = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/?year=2018"))
+    movies = []
+    movie_list.css(".class dropdown-menu").children.css("a").map { |l| l.attribute("href").value }
+    years.each.do |year|
+      movie = year.css("a.unstyled.articleLink").text
+      movies << movie
+    end
+    movies
+  end
+
+  def self.scrape_movies(year_url)
+    movies = []
+    movie_list = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/?year=#{year}"))
+    movie_list.css("a.unstyled.articleLink").map { |l| l.text }
+    movies << movie
   end
 end

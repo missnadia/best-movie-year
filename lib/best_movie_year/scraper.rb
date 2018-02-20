f5cfd74dae81f41d64a8f489bda6cfcb38193fcd
Nadia Lee <miss.nadia.lee@gmail.com>
2018-02-20 18:28:21 +0000

    revise scraper code

--- lib/best_movie_year/scraper.rb
+++ lib/best_movie_year/scraper.rb
@@ -4,12 +4,10 @@ class BestMovies::Scraper
     movie_list = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/?year=2018"))
     movies = []
     movie_list.css("div.btn-group btn-primary-border-dropdown").each do |list|
-      movie_year = list.css("fontelloIcon icon-down-dir pull-right").text
-      years = list.css(".class dropdown-menu").children.css("a").map { |y| y.attribute("href").value }
-        years.each.do |year|
-          movie_name = year.css(".unstyled articleLink").text
-          movies << {year: movie_year, name: movie_name}
-        end
+      list.css(".class dropdown-menu").children.css("a").map { |l| l.attribute("href").value }
+      years.each.do |year|
+        movie = year.css(".unstyled articleLink").text
+        movies << {name: movie}
       end
     end
   end

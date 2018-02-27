cd8bf63dd0f1d2c3c1bdb2490472952cb8d82ff8
Nadia Lee <32117376+missnadia@users.noreply.github.com>
2018-02-27 17:17:45 -0500

    update cli.rb

--- lib/best_movie_year/cli.rb
+++ lib/best_movie_year/cli.rb
@@ -13,7 +13,7 @@ class BestMovies::CLI
     puts "Please enter a four-digit year from 1950 to 2018 to view the top 10 movies of that year. To exit, please type 'exit':"
     input = gets.strip
     if input.to_i >= 1950 && input.to_i <= 2018
-      BestMovies::Movie.find(input.to_i)
+      BestMovies::Movie.find(input)
       puts ""
       puts "Top 10 Movies of #{input.to_i}"
       print_movies

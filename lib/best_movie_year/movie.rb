3d6010823b3c328be8bce843978941d540078040
Nadia Lee <32117376+missnadia@users.noreply.github.com>
2018-02-27 16:26:44 -0500

    update movie.rb

--- lib/best_movie_year/movie.rb
+++ lib/best_movie_year/movie.rb
@@ -1,9 +1,10 @@
 class BestMovies::Movie
-  attr_accessor :title, :year
+  attr_accessor :title, :year, :year_url
 
-  def initialize(title = nil, year = nil)
-    @title = title
-    @year = year
+  @@all = []
+  
+  def initialize
+    years_hash.each { |attribute, value| self.send("#{attribute}=", value) }
     @@all << self
   end
 
@@ -15,13 +16,23 @@ class BestMovies::Movie
     self.all[input]
   end
 
-  def self.scrape_movies
+  def self.scrape_years
     doc = Nokogiri::HTML(open("https://www.rottentomatoes.com/top/bestofrt/?year=2018"))
     movie_years = []
-    movie_year = doc.css(".dropdown-menu").children.css("a").map { |l| l.attribute("href").value }
-    movie_title = doc.css("a.unstyled.articleLink").text
-    movie_years << {title: movie_title, year: movie_year}
+    
+    year_url = doc.css(".dropdown-menu").children.css("a").map { |l| l.attribute("href").value }
+    movie_year = doc.css(".dropdown-menu").children.css("a").map { |y| y.text }
+    movie_years << { year_url: year_url, year: movie_year }
 
     movie_years
+  end
+  
+  
+  def self.scrape_movies(year_url)
+    movies = {}
+    doc = Nokogiri::HTML(open("https://www.rottentomatoes.com#{year_url}"))
+    movies[:title] = doc.css("a.unstyled.articleLink").children.css("a").map { |t| t.text }
+    
+    movies
   end
 end

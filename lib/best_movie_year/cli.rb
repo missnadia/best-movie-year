93413b15da172b7c5090d10ec2647c23a791b500
Nadia Lee <miss.nadia.lee@gmail.com>
2018-03-15 22:15:32 +0000

    add additional user input

--- lib/best_movie_year/cli.rb
+++ lib/best_movie_year/cli.rb
@@ -11,13 +11,55 @@ class BestMovies::CLI
   def make_movies(input)
     movie_array = BestMovies::Movie.scrape_movies(BASE_PATH + "/top/bestofrt/?year=" + input)
     BestMovies::Movie.create(movie_array)
+
+  #  BestMovies::Movie.all.each { |movie|
+  #  add_desc = BestMovies::Movie.scrape_desc(BASE_PATH + "#{movie.url}")
+#    movie.add_value(add_desc)
+#    }
+
   end
 
   def print_movies
     BestMovies::Movie.all.first(10).each.with_index(1) { |movie, i|
         puts "#{i}. #{movie.title[:title]}"
-        puts "#{movie.desc}"
     }
+    puts ""
+    puts "Would you like more information? (Y/N)"
+    input1 = gets.strip
+    if input1.downcase == "y"
+      puts "info"
+      puts ""
+      puts "Would you like to enter a different year? (Y/N)"
+        input2 = gets.strip
+        puts ""
+        if input2.downcase == "n"
+          puts "Goodbye."
+        elsif input2.downcase == "y"
+          start
+        else
+          puts ""
+          puts "Invalid entry. Please enter Y or N"
+          puts ""
+        end
+    elsif input1.downcase == "n"
+      puts ""
+      puts "Would you like to enter a different year? (Y/N)"
+        input1 = gets.strip
+        puts ""
+        if input2.downcase == "n"
+          puts "Goodbye."
+        elsif input2.downcase == "y"
+          start
+        else
+          puts ""
+          puts "Invalid entry. Please enter Y or N"
+          puts ""
+        end
+    else
+      puts ""
+      puts "Invalid entry. Please enter Y or N"
+      print_movies
+    end
   end
 
   def start
@@ -31,6 +73,7 @@ class BestMovies::CLI
     elsif input.downcase == "exit"
       puts "Goodbye."
     else
+      puts ""
       puts "Invalid entry."
       start
     end
--- lib/best_movie_year/movie.rb
+++ lib/best_movie_year/movie.rb
@@ -8,17 +8,17 @@ class BestMovies::Movie
 
   @@all = []
 
-  def initialize(movie_hash)
-    movie_hash.each { |attribute, value|
-      self.send("#{attribute}=", value)
-    }
+  def initialize(title = nil, desc = nil, url = nil)
+    @title = title
+    @desc = desc
+    @url = url
     @@all << self
   end
 
   def self.create(movie_array)
     movie_array.each { |movie_hash|
       @movie = self.new(movie_hash)
-      @movie.add_value(movie_hash)
+      #@movie.add_value(desc_hash)
     }
   end
 
@@ -61,17 +61,16 @@ class BestMovies::Movie
     movies.each { |movie|
       movie_title = movie.text.split("            ").first(11).drop(1)
       movie_url = "https://www.rottentomatoes.com" + movie.attribute("href").value
-      movie_desc = @movie.scrape_desc(movie_url)
-      movie_list << {title: movie_title[0], url: movie_url, desc: movie_desc}
+      movie_list << {title: movie_title[0], url: movie_url}
     }
 
     movie_list
   end
 # Movie.scrape_movies("https://www.rottentomatoes.com/top/bestofrt/?year=2000")
 
-  def self.scrap_desc(url)
+  def self.scrape_desc(url)
     movies = {}
-    doc = Nokogiri::HTML(open(url))
+    doc = Nokogiri::HTML(open("#{url}"))
     movies[:desc] = doc.css("div#movieSynopsis.movie_synopsis.clamp.clamp-6").text.strip
 
     movies
